46071afd38f50f77009da5844269bf58757aed48
caio <caiobruno03@icloud.com>
2022-07-24 13:02:05 -0300

    mudança

--- script.js
+++ script.js
@@ -10,22 +10,18 @@ function verificaPalindromo2(string) {
     
     if (!string) return;
     
-    
-    if (!string) {
-       string.trim() 
-    } 
 
     for (let i = 0; i < string.length / 2 ; i++){
 
         if (string[i] !== string[string.length - 1 - [i]]) return false;
 
         // console para debugar variaveis
-        console.log(string[string.length - 1 - [i]])
+        console.log(string[i] + string[string.length - 1 - [i]])
          // console para debugar variaveis
-        console.log(string.length / 2 + " string / 2")
+     
          // console para debugar variaveis
-        console.log(string[i] + " string [i]")
+       
     }
     return true;
 }
-console.log(verificaPalindromo2("aposasopa"))
+console.log(verificaPalindromo2("agordaamaadroga"))
